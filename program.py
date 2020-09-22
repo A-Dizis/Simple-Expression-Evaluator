6596100e70f8e685014c54de3a387a1c71440f6e
A-Dizis <37675047+A-Dizis@users.noreply.github.com>
2020-09-22 23:41:45 +0300

    Add files via upload

--- program.py
+++ program.py
@@ -2,6 +2,10 @@ import math as mt
 import sys
 from decimal import Decimal
 
+#####################################################
+## SET THE BASIC FUNCTIONS FOR THE OPERATORS       ##
+#####################################################
+
 def Power(num1, num2):
     answer = num1 ** num2
     return answer
@@ -22,6 +26,14 @@ def Subtract(num1, num2):
     answer = num1 - num2
     return answer
 
+####################################################
+## DECLARE THE APPROPRIATE LIST TO RECOGNIZE      ##
+## FUNCTIONS AND NUMBERS                          ##
+##                                                ##
+## FUNCTION DICTIONARIES TO CALL FUNCTIONS        ##
+## THROUGH THEIR STRING NAME                      ##
+####################################################
+
 NUMBERS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.']
 ASCII = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t',\
         'u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O', \
@@ -29,6 +41,11 @@ ASCII = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r'
 FUNCTIONS_1ARG = {'sin': mt.sin, 'cos': mt.cos, 'tan': mt.tan, 'exp': mt.exp}
 FUNCTIONS_2ARG = { 'log': mt.log, '^': Power, '*': Multiply, '/': Divide, '+': Add, '-': Subtract}
 OPERATORS = ['^', '*', '/', '+', '-']
+
+####################################################
+## FUNCTIONS TO PHARSE NUMBERS AND FUNCTION NAMES ##
+## AND GET THE ACTUAL FUNCTIONS                   ##
+####################################################
 
 def get_number(string, i, length):
     number = []
@@ -64,6 +81,10 @@ def return_function(function):
     else:
          return FUNCTIONS_FUNCTIONS_1ARG[function]
 
+###################################################
+## FUNCTIONS TO SET THE PRECEDENCE               ##
+###################################################
+
 def precedence(operator):
     if(operator == '^'):
         return 0
@@ -76,13 +97,27 @@ def precedence(operator):
 
 
 #________________________START PROGRAM________________________#
+
+##################################################
+## GET THE EXPRESSION PURGE THE WHITESPACES AND ##
+## SET THE TWO STACK OF THE ALGORITHM           ##
+##################################################
+
 user_string = input("GIVE VALID EXPRESSION:\n")
 user_string = user_string.replace(' ','')
 
 NUMBER_STACK = []
 OPERATOR_STACK = []
 
+##################################################
+## READ AND RECOGNIZE EACH TERM OF THE EXPRESSI ##
+## ON AND THEN ACT APPROPRIATELY                ##
+## GET CHARACTERS UNTIL THE EXPRESSION STRING H ##
+## AS COMPLETELY PHARSED                        ##
+##################################################
+
 i = 0
+
 while(i < len(user_string)):
     if(user_string[i] in NUMBERS):
         num, i = get_number(user_string, i, len(user_string))
@@ -140,6 +175,7 @@ while(i < len(user_string)):
                 f = FUNCTIONS_2ARG[OPERATOR_STACK.pop()]
                 c = Decimal(f(a, b))
                 NUMBER_STACK.append(c)
+
             else:
                 a = NUMBER_STACK.pop()
                 f = FUNCTIONS_1ARG[OPERATOR_STACK.pop()]
@@ -147,18 +183,21 @@ while(i < len(user_string)):
                 NUMBER_STACK.append(c)
 
         OPERATOR_STACK.pop()
+
         if OPERATOR_STACK:
             if((OPERATOR_STACK[-1] not in OPERATORS) and (OPERATOR_STACK[-1] in FUNCTIONS_1ARG)):
                 a = NUMBER_STACK.pop()
                 f = FUNCTIONS_1ARG[OPERATOR_STACK.pop()]
                 c = Decimal(f(a))
                 NUMBER_STACK.append(c)
+
             elif((OPERATOR_STACK[-1] not in OPERATORS) and (OPERATOR_STACK[-1] in FUNCTIONS_2ARG)):
                 b = NUMBER_STACK.pop()
                 a = NUMBER_STACK.pop()
                 f = FUNCTIONS_2ARG[OPERATOR_STACK.pop()]
                 c = Decimal(f(a, b))
                 NUMBER_STACK.append(c)
+
             else:
                 pass
 
@@ -172,6 +211,13 @@ while(i < len(user_string)):
 
     if(i >= len(user_string)):
         break
+
+###################################################
+## IF AN ERROR ON THE FOLLOWING PART OF THE CODE ##
+## IT MEANS THAT USER HAS GIVEN MISSALIGNED PARE ##
+## NTHESIS, SO TERMINATE WITH ERROR?             ##
+###################################################
+
 try:
     while(OPERATOR_STACK):
         if(OPERATOR_STACK[-1] in FUNCTIONS_2ARG):
@@ -191,5 +237,10 @@ except:
     print("MISALLIGNED PARENTHESES. EXITING...\n")
     input()
     sys.exit(-1)
+
+###################################################
+## PRINT THE FINAL RESULT AFTER THE OPERATOR STA ##
+## CK EMPTYIED ITS CONTENTS                      ##
+###################################################
 
 print(NUMBER_STACK.pop())
